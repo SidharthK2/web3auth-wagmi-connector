1bb36cf4e5e026bc808e320ff0ef55c6bdf6bb99
SidharthK2 <sidharthkapoor.100@gmail.com>
2024-09-25 12:49:39 +0530

    run lint

--- src/lib/connector.ts
+++ src/lib/connector.ts
@@ -41,8 +41,8 @@ export function Web3AuthConnector(parameters: Web3AuthConnectorParams) {
           } else if (loginParams) {
             await web3AuthInstance.connectTo(WALLET_ADAPTERS.AUTH, loginParams);
           } else {
-//prettier-ignore
-  //eslint-disable-next-line
+            //prettier-ignore
+            //eslint-disable-next-line
             console.error("please provide valid loginParams when using @web3auth/no-modal");
             log.error("please provide valid loginParams when using @web3auth/no-modal");
             throw new UserRejectedRequestError(new Error("please provide valid loginParams when using @web3auth/no-modal"));
@@ -62,8 +62,8 @@ export function Web3AuthConnector(parameters: Web3AuthConnectorParams) {
 
         return { accounts, chainId: currentChainId };
       } catch (error) {
-//prettier-ignore
-  //eslint-disable-next-line
+        //prettier-ignore
+        //eslint-disable-next-line
         console.error('error while connecting', error);
         log.error("error while connecting", error);
         this.onDisconnect();
@@ -95,8 +95,8 @@ export function Web3AuthConnector(parameters: Web3AuthConnectorParams) {
         } else if (loginParams) {
           await web3AuthInstance.init();
         } else {
-//prettier-ignore
-  //eslint-disable-next-line
+          //prettier-ignore
+          //eslint-disable-next-line
           console.error("please provide valid loginParams when using @web3auth/no-modal");
           log.error("please provide valid loginParams when using @web3auth/no-modal");
           throw new UserRejectedRequestError(new Error("please provide valid loginParams when using @web3auth/no-modal"));
@@ -132,13 +132,13 @@ export function Web3AuthConnector(parameters: Web3AuthConnectorParams) {
             ? `https://images.toruswallet.io/${chain.nativeCurrency?.symbol.toLowerCase()}.svg`
             : "https://images.toruswallet.io/eth.svg",
         });
-//prettier-ignore
-  //eslint-disable-next-line
+        //prettier-ignore
+        //eslint-disable-next-line
         console.info("Chain Added: ", chain.name);
         log.info("Chain Added: ", chain.name);
         await web3AuthInstance.switchChain({ chainId: `0x${chain.id.toString(16)}` });
-//prettier-ignore
-  //eslint-disable-next-line
+        //prettier-ignore
+        //eslint-disable-next-line
         console.log("Chain Switched to ", chain.name);
         log.info("Chain Switched to ", chain.name);
         config.emitter.emit("change", {
@@ -146,8 +146,8 @@ export function Web3AuthConnector(parameters: Web3AuthConnectorParams) {
         });
         return chain;
       } catch (error: unknown) {
-//prettier-ignore
-  //eslint-disable-next-line
+        //prettier-ignore
+        //eslint-disable-next-line
         console.error("Error: Cannot change chain", error);
         log.error("Error: Cannot change chain", error);
         throw new SwitchChainError(error as Error);
